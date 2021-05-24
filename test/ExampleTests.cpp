da7f50eb62df2465ae74dd43b692f9e808c948c0
Daniel Litt <daniel_litt@comcast.net>
2021-05-24 19:23:23 -0400

    Add function and test for MAC

--- test/ExampleTests.cpp
+++ test/ExampleTests.cpp
@@ -1,5 +1,18 @@
+#include "../Example.hpp"
+
 #include <gtest/gtest.h>
 
-TEST(ExampleTests, DemonstateGTestMacros){
-    EXPECT_EQ(true, true);
+TEST(ExampleTests, MAC){
+    int x = 42;
+    int y = 16;
+    int sum = 100;
+    int expectedNewSum = sum + x * y;
+    EXPECT_EQ(
+        expectedNewSum,
+        MAC(x, y, sum)
+    );
+    EXPECT_EQ(
+        expectedNewSum,
+        sum
+    );
 }
